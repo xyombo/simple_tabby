f98357d22b318c3b3b478d447ac08ac214852297
yombo <yombo@qq.com>
2022-11-06 20:47:19 +0800

    intergrate with argparse

--- simple_tabby/terminal.py
+++ simple_tabby/terminal.py
@@ -1,8 +1,7 @@
 import json
 import os
 import sys
-import time
-
+import argparse
 import pyperclip
 
 configs = json.loads(
@@ -28,28 +27,33 @@ def select_one_to_login(no):
     user = c['user']
     private_key = os.path.expanduser("~/.ssh/id_rsa")
     if 'privateKey' in c:
-        private_key = c
+        private_key = c['privateKey']
     passwd = c['password']
-    pyperclip.copy(passwd)
+    if (passwd):
+        pyperclip.copy(passwd)
+    
+
     print("trying to login server with ssh-key, if you has config password, the password has copied to clip, just pasted and enter!!")
+
     os.system(f"ssh {user}@{host} -p {port} -i {private_key}")
 
 
 def main():
-    argv_len = len(sys.argv)
-    no = 0
-    if argv_len == 2:
-        no = sys.argv[1]
-    else:
+    parser = argparse.ArgumentParser(description="Hello ", add_help=True)
+    parser.add_argument('-ls', '--list', required=False,
+                        action='store_true', help="list all configed servers")
+    parser.add_argument('-c', '--connect', type=str,
+                        required=False, help="connect to remote server by no")
+
+    
+
+    args = parser.parse_args()
+
+    if (args.list):
         # list all remote server machine
         list_all_remote_servers()
-        configs_len = len(configs)
-        print(
-            f"please select one to connect [1,{configs_len}] (input 0 for cancel) :", end="")
-        no = input()
-    if 0 >= int(no):
-        sys.exit()
-    select_one_to_login(int(no))
+    if (args.connect):
+        select_one_to_login(args.connect)
 
 
 if __name__ == '__main__':
