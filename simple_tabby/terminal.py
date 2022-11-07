e47ba89e18feb1e1a99bca9ae0eeb6bfb3880d74
yombo <yombo@qq.com>
2022-11-07 12:28:10 +0800

    suport ssh tunnet

--- simple_tabby/terminal.py
+++ simple_tabby/terminal.py
@@ -4,15 +4,24 @@ import sys
 import argparse
 import pyperclip
 
-configs = json.loads(
-    open(os.path.expanduser("~/.simple_tabby/terminals_config.json"), encoding='utf-8').read())
+DEFAULT_CONFIG_PATH = os.path.expanduser("~/.simple_tabby")
+
+SSH_SERVER_CONFIGS = []
+
+config_files = [config_file for config_file in os.listdir(
+    DEFAULT_CONFIG_PATH) if config_file.endswith(".json")]
+
+for config_file in config_files:
+    configs = json.loads(
+        open(os.path.join(DEFAULT_CONFIG_PATH, config_file), encoding='utf-8').read())
+    SSH_SERVER_CONFIGS = SSH_SERVER_CONFIGS+configs
 
 
 def list_all_remote_servers():
     from prettytable import PrettyTable
     table = PrettyTable(['no', 'name', 'ip', 'apps', 'tags', 'description'])
-    for i in range(0, len(configs)):
-        c = configs[i]
+    for i in range(0, len(SSH_SERVER_CONFIGS)):
+        c = SSH_SERVER_CONFIGS[i]
         apps = ','.join(c['application'])
         tags = ','.join(c['tags'])
         describe = c['describe']
@@ -20,8 +29,8 @@ def list_all_remote_servers():
     print(table)
 
 
-def select_one_to_login(no):
-    c = configs[no - 1]
+def select_one_to_login(no, tunne):
+    c = SSH_SERVER_CONFIGS[no - 1]
     host = c['host']
     port = c['port']
     user = c['user']
@@ -31,11 +40,14 @@ def select_one_to_login(no):
     passwd = c['password']
     if (passwd):
         pyperclip.copy(passwd)
-    
 
     print("trying to login server with ssh-key, if you has config password, the password has copied to clip, just pasted and enter!!")
 
-    os.system(f"ssh {user}@{host} -p {port} -i {private_key}")
+    if (tunne):
+        ports = tunne.split(":")
+        os.system(f"ssh -L {ports[0]}:127.0.0.1:{ports[1]} {user}@{host}")
+    else:
+        os.system(f"ssh {user}@{host} -p {port} -i {private_key}")
 
 
 def main():
@@ -44,16 +56,16 @@ def main():
                         action='store_true', help="list all configed servers")
     parser.add_argument('-c', '--connect', type=int,
                         required=False, help="connect to remote server by no")
-
-    
+    parser.add_argument('-t', '--tunnet', type=str, required=False,
+                        help="open tunnet with ssh localport:remoteport")
 
     args = parser.parse_args()
-
+    print(args)
     if (args.list):
         # list all remote server machine
         list_all_remote_servers()
     if (args.connect):
-        select_one_to_login(args.connect)
+        select_one_to_login(args.connect, args.tunnet)
 
 
 if __name__ == '__main__':
